c09c20c5cbc5af2fa8a7b7e9d578e3c0b8a3fdef
Роберт <dificalabc@gmail.com>
2024-11-08 18:47:36 +0500

    qwe

---
+++ drat.py
@@ -0,0 +1,6 @@
+from pprint import pprint
+
+file = open('example.txt', 'a')
+a = 'hello world'
+if a in file:
+    print('ecte')
---
+++ module_7_1.py
@@ -0,0 +1,42 @@
+from pprint import pprint
+
+
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        content = file.read()
+        file.close()
+        return content
+
+    def add(self, *products):
+        file = open(self.__file_name, 'a')
+        for i in products:
+            if i.name in self.get_products():
+                print(f'Продукт {i.name} уже есть в магазине')
+            else:
+                file.write(f'{i.name}, {i.weight}, {i.category}\n')
+
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
